d78569f99978f6795e46456e360539e018b1be14
Jixun Moe <5713045+jixunmoe@users.noreply.github.com>
2025-06-12 01:30:32 +0100

    chore: add missing types

--- index.d.ts
+++ index.d.ts
@@ -3,3 +3,17 @@
  * @param text input text
  */
 export function zzcSign(text: string): string;
+
+/**
+ * Encrypt a JSON string to Base64 encoded payload for "ag-1" encoded request.
+ * @param data JSON string to encode
+ * @returns Base64 encoded payload
+ */
+export async function encodeAG1Request(data: string): Promise<string>;
+
+/**
+ * Decrypt response from "ag-1" encoded request.
+ * @param data ArrayBuffer or Uint8Array containing the encrypted response
+ * @returns Decrypted response, typically a JSON string
+ */
+export function decodeAG1Response(data: ArrayBuffer | Uint8Array): string;
